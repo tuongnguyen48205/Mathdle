1f813f8f172a63258b01b1f043df65882d1c4883
Tuong Bao Nguyen <124882892+tuongnguyen48205@users.noreply.github.com>
2023-08-19 17:05:17 +1000

    Update FINAL GAME - MATHDLE.py

--- FINAL GAME - MATHDLE.py
+++ FINAL GAME - MATHDLE.py
@@ -311,6 +311,117 @@ def create_guess(all_info, difficulty=DEF_DIFFIC):
 
     return "".join(mylist)
 
+def passes_restrictions(guess, all_info):
+    '''
+    Tests a `guess` equation against `all_info`, a list of known restrictions, 
+    one entry in that list from each previous call to set_colors(). Returns 
+    True if that `guess` complies with the collective evidence imposed by 
+    `all_info`; returns False if any violation is detected. Does not check the 
+    mathematical accuracy of the proposed candidate equation.
+    '''
+  
+    move_info = []  # Stores all the statuses and positions of the character.
+    greenlist = []  # Stores all the info of the moves with 'green' status
+    counter = -1  # To keep track of index in the guess
+    
+    # The following chunk of code is used to create a list with every move,
+    # including the position ('counter'), the actual character and a  
+    # placeholder 'colour', based off of the guess.
+    for character in guess:
+        counter += 1
+        move_info.append((counter, character, 'colour'))
+    
+    # The following is for the positional constraint.
+    
+    # The following chunk of code goes through each character in the guess
+    # and compares it to all the information gathered thus far. If the
+    # guess character is present in all the information and has the same 
+    # position and has the status 'grey' or 'yellow', the function returns 
+    # False as that does not pass restrictions. However, if the function has
+    # status 'green', it is placed into the 'greenlist' where it is handled
+    # later.
+    for move in move_info:
+        for info in all_info:
+            for item in info:
+                if item[0] == move[0] and item[1] == move[1]:
+                    if item[2] == GREYY:
+                        return False
+                    elif item[2] == YELLO:
+                        return False
+                    elif item[2] == GREEN:
+                        greenlist.append(item)
+                        
+    # This goes through all the information available. If not all 'green' 
+    # status moves are present in the guess, the function returns False 
+    # as it does not pass restrictions.
+    for info in all_info:
+        for item in info:
+            if item[2] == GREEN:
+                if item not in greenlist:
+                    return False
+    
+    # The following is for distributional constraint.
+        
+    guess_tracker = []  # This keeps track of the guesses
+    greytags = 0  # This is the number of 'grey' status characters in all_info
+    
+    # The following chunk of code goes through all possible FoCdle characters.
+    # It tracks the status of the previous guess in all_info, counting the 
+    # number  of 'grey', 'green', and 'yellow' tags it has, before placing
+    # all this info into the guess_tracker.
+    for character in "0123456789+-*%=":
+        greencount = 0
+        yellowcount = 0
+        greycount = 0
+        try:
+            for item in all_info[-1]:
+                if item[2] == GREYY:
+                    greytags += 1
+                if character in item and item[2] == GREEN:
+                    greencount += 1
+                if character in item and item[2] == YELLO:
+                    yellowcount += 1
+                if character in item and item[2] == GREYY:
+                    greycount += 1  
+        except IndexError:
+            continue
+        guess_tracker.append((character, greencount, yellowcount, greycount))
+    
+    # The following tally is used to count the number of times each character
+    # appears in the guess.
+    tally = {}
+    for character in guess:
+        if character in tally:
+            tally[character] += 1
+        else:
+            tally[character] = 1
+    
+    # The following chunk of code is the actual distributional constraint.
+    # Essentially, the function will return False if the the number of times
+    # a character in the guess is less than the sum of that character's 'green'
+    # and 'yellow' counts in the information or if the number of times a 
+    # character in the guess is greater than the sum of that character's 
+    # 'green' and 'yellow' counts in the information if that character has one
+    # or more 'grey' tags in the information. It will also return False if the
+    # number of times a character in the guess is less than the sum of that
+    # character's 'green' and 'yellow' counts in the information plus the total
+    # number of 'grey' tags in the information if that character doesn't have
+    # any 'grey' tags in the information.
+    for keys, values in tally.items():
+        for possible_char in guess_tracker:
+            if keys in possible_char:
+                if values < possible_char[1] + possible_char[2]:
+                    return False
+                if possible_char[3] >= 1:
+                    if values > possible_char[1] + possible_char[2]:
+                        return False
+                if possible_char[3] == 0:
+                    if values > possible_char[1] + possible_char[2] + greytags:
+                        return False
+     
+    # If all the conditions are met, it means that guess passes restrictions
+    # and so the function returns True.
+    return True
 
 #--------------------------------------------------------------
 
@@ -538,6 +649,12 @@ elif make_own == "random":
         print("\n------------------------------------------------------------------------------------")
         print(f"\nYour Mathdle has been generated: {original}")
 
+print('\nYou can type "quit" at any time to exit, "info" for all informtation or')
+print('"guesses" to show all guesses you have made so far or even "guess_info" to')
+print("show all the information related to the guesses you have made so far,")
+print('or "restart" to restart the game.')
+
+guesses_made = []
 similarity_list = []
 while True:
     # The following line is for if the player guessed the secret correctly
@@ -580,6 +697,27 @@ while True:
     while True:
         try:
             guess = input("\nPlease enter your guess (it must be the same length as the difficulty chosen): ")
+            if guess == "quit":
+                print("\n------------------------------------------------------------------------------------")
+                print("\nThank you for playing!")
+                print("\n------------------------------------------------------------------------------------")
+                exit()
+            elif guess == "guesses":
+                print("\n------------------------------------------------------------------------------------\n")
+                for i in guesses_made:
+                    print(i)
+                print("\n------------------------------------------------------------------------------------\n")
+                raise ValueError
+            elif guess == "guess_info":
+                print("\n------------------------------------------------------------------------------------\n")
+                print(all_info)
+                print("\n------------------------------------------------------------------------------------\n")
+                raise ValueError
+           # elif guess == "info":
+                # put info here
+            else:
+                pass
+            
             guess_list = []
             for item in guess:
                 if item in DIGITS or item in EQUALITY or item in OPERATORS:
@@ -592,13 +730,14 @@ while True:
                 raise Endturn
         except Endturn:
             print("Your guess is invalid.")
+        except ValueError:
+            pass
      
     # Now that we have our guess, we can compare the guess we made to the secret
     guess_counter += 1 #Increases the number of guesses made by 1
     similarity = set_colors(secret, guess)
     similarity_list = []
     for colour in similarity:
-        all_info.append(colour)
         if colour[-1] == GREEN:
             similarity_list.append("G")
         elif colour[-1] == YELLO:
@@ -611,6 +750,18 @@ while True:
     print(f"For guess number {guess_counter}, here is your outcome:\n")
     print(guess)
     print(''.join(similarity_list))
+    # Now let the player know if the guess that they made was a good one or a bad one
+    good_guess = passes_restrictions(guess, all_info)
+    if good_guess == True:
+        print("\nThat was a good guess! Keep it up!")
+    elif good_guess == False:
+        print("\nNot the best guess. Better luck with the next one.")
+
+    # Some final stuff: just add the guess to the bank of knowledge
+    move = (guess, ''.join(similarity_list))
+    guesses_made.append(move)
+    all_info.append(set_colors(secret, guess))
+    
 
 print("\n------------------------------------------------------------------------------------\n")
 print("Congratulations! You have solved the Mathdle!")
@@ -633,6 +784,7 @@ while True:
     else:
         continue
 
+# upload intro sequence
 #change information
 # need to add colours and print board as stuff reveals
 # show all guesses
